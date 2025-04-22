38461975780e8cc9be123824eaaa955398c22f48
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-23 04:38:48 +0800

    Update lcd_test.py

--- lcd_test.py
+++ lcd_test.py
@@ -1,11 +1,11 @@
 # lcd_test.py
 from luma.core.interface.serial import spi
-from luma.lcd.device import st7735  # 或 st7789 看你实际用的
+from luma.lcd.device import st7789
 from PIL import Image
 import time
 
 serial = spi(port=0, device=0, gpio_DC=24, gpio_RST=25)
-device = st7735(serial, width=128, height=160)
+device = st7789(serial, width=240, height=240)  # 根据你的实际分辨率
 
 frames = ["slime1.png", "slime2.png", "slime3.png"]  # 替换为你准备的动图帧
 
