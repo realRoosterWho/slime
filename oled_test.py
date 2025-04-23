f64595208ca0f74802f2edfb49bda2f919330a4a
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-23 23:57:00 +0800

    Update oled_test.py

--- oled_test.py
+++ oled_test.py
@@ -1,10 +1,10 @@
 from luma.core.interface.serial import i2c
-from luma.oled.device import ssd1315
+from luma.oled.device import ssd1306
 from luma.core.render import canvas
 from PIL import ImageFont
 
 serial = i2c(port=1, address=0x3C)
-device = ssd1315(serial, width=128, height=64)
+device = ssd1306(serial, width=128, height=64)
 
 with canvas(device) as draw:
     draw.text((10, 10), "Hello Slime!", fill="white")
