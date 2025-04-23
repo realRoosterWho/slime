8cd74f3e23e03ea196632424e62f62a71708e7ba
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-24 03:42:56 +0800

    Create record_test.py

---
+++ record_test.py
@@ -0,0 +1,15 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import os
+
+samplerate = 44100  # 采样率
+duration = 5        # 录音时长（秒）
+channels = 1        # 单声道
+filename = "mic_test.wav"
+
+print(f"🎤 开始录音（{duration} 秒）...")
+recording = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=channels, dtype='int16', device=1)
+sd.wait()
+
+write(filename, samplerate, recording)
+print(f"✅ 录音完成，保存为 {filename}")
