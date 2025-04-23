81ae115fa39c4568f3ea97576ec3b64f340eb3d7
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-24 05:53:04 +0800

    Update oled_test.py

--- oled_test.py
+++ oled_test.py
@@ -10,7 +10,7 @@ oled = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 # 创建图像
 image = Image.new("1", (128, 64))
 draw = ImageDraw.Draw(image)
-draw.text((10, 10), "( ；´Д｀)i just wanna die...qwq", fill=255)
+draw.text((10, 10), "uwu i just wanna die...qwq", fill=255)
 
 # 显示图像
 oled.image(image)
