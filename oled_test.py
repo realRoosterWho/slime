288ff4f9e1988071b670fdcdb1b25a95bf30e810
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-23 23:57:49 +0800

    Update oled_test.py

--- oled_test.py
+++ oled_test.py
@@ -1,12 +1,15 @@
+import time
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
-from luma.core.render import canvas
-from PIL import ImageFont
 
+print("准备初始化 OLED...")
 serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial, width=128, height=64)
+device = ssd1306(serial)
+print("初始化完成，开始发送数据...")
 
-with canvas(device) as draw:
-    draw.text((10, 10), "Hello Slime!", fill="white")
+for i in range(100):
+    device.clear()
+    device.show()
+    time.sleep(1)
 
-print("Displaying image...")
+print("完成")
