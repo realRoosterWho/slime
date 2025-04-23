720cd5d3a04811c1403773e83cb3f669686d8810
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-23 23:55:51 +0800

    Update oled_test.py

--- oled_test.py
+++ oled_test.py
@@ -1,15 +1,12 @@
 from luma.core.interface.serial import i2c
-from luma.oled.device import ssd1306
-from PIL import Image, ImageDraw
+from luma.oled.device import ssd1315
+from luma.core.render import canvas
+from PIL import ImageFont
 
 serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial, width=128, height=64)
+device = ssd1315(serial, width=128, height=64)
 
-# 设置对比度最大
-device.contrast(255)
+with canvas(device) as draw:
+    draw.text((10, 10), "Hello Slime!", fill="white")
 
-# 创建一个全白的图像
-img = Image.new("1", (128, 64), color=1)
-device.display(img)
-
-print("试图强制点亮整个屏幕")
+print("Displaying image...")
