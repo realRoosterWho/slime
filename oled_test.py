d3f4d8932bbf788138986cf42943868424fe88b5
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-23 23:59:32 +0800

    Update oled_test.py

--- oled_test.py
+++ oled_test.py
@@ -1,15 +1,17 @@
-import time
-from luma.core.interface.serial import i2c
-from luma.oled.device import ssd1306
+import board
+import busio
+from adafruit_ssd1306 import SSD1306_I2C
+from PIL import Image, ImageDraw
 
-print("准备初始化 OLED...")
-serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial)
-print("初始化完成，开始发送数据...")
+# 初始化I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 
-for i in range(100):
-    device.clear()
-    device.show()
-    time.sleep(1)
+# 创建图像
+image = Image.new("1", (128, 64))
+draw = ImageDraw.Draw(image)
+draw.text((10, 10), "Hello Slime!", fill=255)
 
-print("完成")
+# 显示图像
+oled.image(image)
+oled.show()
