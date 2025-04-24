62424beec55eca1f9a88109d7d57c21941bbe4fe
RoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-25 04:11:43 +0800

    Update lcd_test.py

--- lcd_test.py
+++ lcd_test.py
@@ -7,7 +7,7 @@ import time
 serial = spi(port=0, device=0, gpio_DC=24, gpio_RST=25)
 device = st7789(serial, width=240, height=240)  # 根据你的实际分辨率
 
-# frames = ["slime1.png", "slime2.png", "slime3.png"]  # 替换为你准备的动图帧
+frames = ["slime1.png", "slime2.png", "slime3.png"]  # 替换为你准备的动图帧
 # 这次用test_1.jpg到test_10.jpg
 frames = [f"test_{i+1}.jpg" for i in range(10)]
 
