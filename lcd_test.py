697cdf6f24b64e1f10628f74c56b9db2a430daaf
realRoosterWho <gonghq@shanghaitech.edu.cn>
2025-04-28 23:51:44 +0800

    word

--- lcd_test.py
+++ lcd_test.py
@@ -9,7 +9,7 @@ device = st7789(serial, width=240, height=240)  # 根据你的实际分辨率
 
 frames = ["slime1.png", "slime2.png", "slime3.png"]  # 替换为你准备的动图帧
 # 这次用test_1.jpg到test_10.jpg
-frames = [f"test_{i+1}.jpg" for i in range(10)]
+# frames = [f"test_{i+1}.jpg" for i in range(10)]
 
 while True:
     for frame in frames:
